1f063fa356b7b43b65deffb555f11fbc933e1816
Sukasa <nick@greenfirework.com>
2025-10-11 21:43:16 -0700

    Add stationary battery patches

---
+++ Assets/Scripts/patches/StationaryBatteryPatches.cs
@@ -0,0 +1,24 @@
+﻿using Assets.Scripts.Objects.Electrical;
+using HarmonyLib;
+using System;
+
+namespace ReVolt.Assets.Scripts.patches
+{
+    [HarmonyPatch(typeof(Battery))]
+    internal class StationaryBatteryPatches
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(Battery.GetUsedPower))]
+        public static void LimitMaxChargeRate(Battery __instance, ref float __result)
+        {
+            __result = MathF.Min(__result, __instance.PowerMaximum * ReVolt.configMaxBatteryChargeRate.Value);
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(Battery.GetGeneratedPower))]
+        public static void LimitMaxDischargeRate(Battery __instance, ref float __result)
+        {
+            __result = MathF.Min(__result, __instance.PowerMaximum * ReVolt.configMaxBatteryDischargeRate.Value);
+        }
+    }
+}
