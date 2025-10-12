afa7f2fa33696f9a3694ed418df2c49d50de1c1a
Sukasa <nick@greenfirework.com>
2025-10-11 21:42:47 -0700

    Cleanup of the cable network patches

--- Assets/Scripts/patches/CableNetworkPatches.cs
+++ Assets/Scripts/patches/CableNetworkPatches.cs
@@ -1,44 +1,31 @@
-﻿using Assets.Scripts;
-using Assets.Scripts.Networks;
+﻿using Assets.Scripts.Networks;
 using Assets.Scripts.Objects.Electrical;
 using HarmonyLib;
 using System;
+using System.Reflection;
 
 namespace ReVolt.Assets.Scripts.patches
 {
     [HarmonyPatch(typeof(CableNetwork))]
     public class CableNetworkPatches
     {
-        [HarmonyPostfix]
-        [HarmonyPatch(nameof(CableNetwork.DirtyPowerAndDataDeviceLists))]
+        private static readonly FieldInfo TickSetter = typeof(CableNetwork).GetField(nameof(CableNetwork.PowerTick));
+
+        [HarmonyPostfix, HarmonyPatch(nameof(CableNetwork.DirtyPowerAndDataDeviceLists))]
         public static void DirtyListsPatch(CableNetwork __instance)
         {
-            if (__instance.PowerTick is not RevoltTick revoltTick)
-                return;
-
-            revoltTick.IsDirty = true;
+            if (__instance.PowerTick is RevoltTick revoltTick)
+                revoltTick.IsDirty = true;
         }
 
-        [HarmonyPostfix]
-        [HarmonyPatch(MethodType.Constructor, new Type[0])]
-        public static void Constructor_None(CableNetwork __instance)
-        {
-            Inject(__instance);
-        }
+        [HarmonyPostfix, HarmonyPatch(MethodType.Constructor, new Type[0])]
+        public static void Constructor_None(CableNetwork __instance) => Inject(__instance);
 
-        [HarmonyPostfix]
-        [HarmonyPatch(MethodType.Constructor, new Type[1] { typeof(Cable) })]
-        public static void Constructor_Cable(CableNetwork __instance)
-        {
-            Inject(__instance);
-        }
+        [HarmonyPostfix, HarmonyPatch(MethodType.Constructor, new Type[1] { typeof(Cable) })]
+        public static void Constructor_Cable(CableNetwork __instance) => Inject(__instance);
 
-        [HarmonyPostfix]
-        [HarmonyPatch(MethodType.Constructor, new Type[1] { typeof(long) })]
-        public static void Constructor_Long(CableNetwork __instance)
-        {
-            Inject(__instance);
-        }
+        [HarmonyPostfix, HarmonyPatch(MethodType.Constructor, new Type[1] { typeof(long) })]
+        public static void Constructor_Long(CableNetwork __instance) => Inject(__instance);
 
         /// <summary>
         ///     Injection function that replaces the default <seealso cref="PowerTick" /> class with a <seealso cref="RevoltTick" /> instance
@@ -46,16 +33,6 @@ namespace ReVolt.Assets.Scripts.patches
         /// <param name="networkInstance">
         ///     The <seealso cref="CableNetwork"/> to inject the <seealso cref="RevoltTick"/> into
         /// </param>
-        private static void Inject(CableNetwork networkInstance)
-        {
-            ConsoleWindow.Print("Replacing powertick instance");
-            if (networkInstance.PowerTick is not RevoltTick)
-            {
-                // PowerTick is a readonly variable, so use reflection to ignore that
-                var fieldInfo = typeof(CableNetwork).GetField(nameof(CableNetwork.PowerTick));
-                fieldInfo.SetValue(networkInstance, new RevoltTick());
-                ConsoleWindow.Print("Replaced powertick instance!");
-            }
-        }
+        private static void Inject(CableNetwork networkInstance) => TickSetter.SetValue(networkInstance, new RevoltTick());
     }
 }
