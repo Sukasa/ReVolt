fd25808b20d108561336f9034fb87a74fa53cce9
Sukasa <nick@greenfirework.com>
2025-10-19 13:08:45 -0700

    Moved and refactored load center interface used by power tick

---
+++ Assets/Scripts/Interfaces/ILoadCenter.cs
@@ -0,0 +1,18 @@
+﻿using Assets.Scripts.Networks;
+using static ReVolt.RevoltTick;
+
+namespace ReVolt.Interfaces
+{
+    public interface ILoadCenter
+    {
+        public bool HasConflict { get; set; }
+
+        public PowerUsage[] LoadControlData { set; }
+
+        public bool EnableLights { get; }
+        public bool EnableDoors { get; }
+        public bool EnableAtmos { get; }
+        public bool EnableEquip { get; }
+        public bool EnableLogic { get; }
+    }
+}
