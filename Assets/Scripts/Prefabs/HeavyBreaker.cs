eec8760f5b26b3128e4e155e34bb57822298dfb6
Sukasa <nick@greenfirework.com>
2025-10-28 20:03:55 -0700

    More work on heavy breaker

--- Assets/Scripts/Prefabs/HeavyBreaker.cs
+++ Assets/Scripts/Prefabs/HeavyBreaker.cs
@@ -3,6 +3,7 @@ using LibConstruct;
 using ReVolt.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using static ReVolt.Interfaces.ISwitchgearComponent;
 
 namespace ReVolt.Prefabs
@@ -15,7 +16,17 @@ namespace ReVolt.Prefabs
 
         PseudoNetwork<ISwitchgearComponent> IPseudoNetworkMember<ISwitchgearComponent>.Network { get; }
 
-        public IEnumerable<Connection>  Connections
+        private List<long> PowerEntries;
+        private List<long> DataEntries;
+
+        private bool DirtyLists = true;
+
+        private readonly long[] ConnectionRefIds = new long[3];
+        private readonly int[] ConnectionIndices = new int[3] { 0, 0, 0 };
+
+        private static readonly float[] SettingOffsets = new float[4] { -10000.0f, -1000.0f, 1000.0f, 10000.0f };
+
+        public IEnumerable<Connection> Connections
         {
             get
             {
@@ -25,19 +36,91 @@ namespace ReVolt.Prefabs
             }
         }
 
+        public override DelayedActionInstance InteractWith(Interactable interactable, Interaction interaction, bool doAction = true)
+        {
+            var action = new DelayedActionInstance
+            {
+                Duration = 0f,
+                ActionMessage = interactable.ContextualName,
+            };
+
+            switch (interactable.Action)
+            {
+                case InteractableType.Button6: // Cycle Input Conn.
+                case InteractableType.Button7: // Cycle Output Conn.
+                case InteractableType.Button8: // Cycle Data Conn.
+                    UpdateEntryLists();
+
+                    int conIdx = (int)interactable.Action - (int)InteractableType.Button6;
+                    int dir = 1;
+                    if (KeyManager.GetButton(KeyMap.QuantityModifier))
+                        dir = -1;
+
+                    List<long> conList = interactable.Action == InteractableType.Button8 ? DataEntries : PowerEntries;
+                    int nextIdx = (conIdx + dir + conList.Count) % conList.Count;
+
+                    if (!doAction)
+                        return action.Fail("Not Implemented");
+
+
+
+                    return action.Fail("Not Implemented");
+
+                case InteractableType.Button9:  // Setting buttons
+                case InteractableType.Button10:
+                case InteractableType.Button11:
+                case InteractableType.Button12:
+                    int adjIdx = (int)interactable.Action - (int)InteractableType.Button9;
+                    float by = SettingOffsets[adjIdx];
+
+                    return action.Fail("Not Implemented");
+
+                // TODO cases for the four setting buttons
+
+                default:
+                    return base.InteractWith(interactable, interaction, doAction);
+            }
+        }
+
+        private void UpdateEntryLists(bool Force = false) { 
+            if (Force || !DirtyLists)
+                return;
+            DirtyLists = false;
+
+            PowerEntries = SwitchgearNetwork.MemberNetwork(this).Members.Where(x => x.ComponentType == SwitchgearComponentType.Power).Select(x => x.ReferenceId).ToList();
+            DataEntries = SwitchgearNetwork.MemberNetwork(this).Members.Where(x => x.ComponentType == SwitchgearComponentType.Data).Select(x => x.ReferenceId).ToList();
+            ConnectionIndices[0] = Math.Max(PowerEntries.IndexOf(ConnectionRefIds[0]), 0);
+            ConnectionIndices[1] = Math.Max(DataEntries.IndexOf(ConnectionRefIds[1]), 0);
+            ConnectionIndices[2] = Math.Max(PowerEntries.IndexOf(ConnectionRefIds[2]), 0);
+        }
+
+        private void UpdateConnections()
+        {
+
+        }
+
+        protected override void CheckConnections()
+        {
+
+        }
+
         public void OnMembersChanged()
         {
+            DirtyLists = true;
+        }
+
+        public void OnMemberAdded(ISwitchgearComponent member)
+        {
 
         }
 
-        void IPseudoNetworkMember<ISwitchgearComponent>.OnMemberAdded(ISwitchgearComponent member)
+        public void OnMemberRemoved(ISwitchgearComponent member)
         {
-            
-        }
-
-        void IPseudoNetworkMember<ISwitchgearComponent>.OnMemberRemoved(ISwitchgearComponent member)
-        {
-            
+            if (ConnectionRefIds.Contains(member.ReferenceId))
+            {
+                UpdateEntryLists(true);
+                UpdateConnections();   
+            }
         }
     }
 }
