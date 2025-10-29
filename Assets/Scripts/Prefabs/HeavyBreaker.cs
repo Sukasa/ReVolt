7181913de27d8f0c32e08c534a7b1c202b7a8c91
Sukasa <nick@greenfirework.com>
2025-10-28 19:02:32 -0700

    Add Heavy Breaker, first drafts

---
+++ Assets/Scripts/Prefabs/HeavyBreaker.cs
@@ -0,0 +1,44 @@
+﻿using Assets.Scripts.Objects;
+using LibConstruct;
+using ReVolt.Interfaces;
+using System;
+using System.Collections.Generic;
+using static ReVolt.Interfaces.ISwitchgearComponent;
+
+namespace ReVolt.Prefabs
+{
+    public class HeavyBreaker : CircuitBreaker, ISwitchgearComponent
+    {
+        public static PseudoNetworkType<SwitchgearNetwork> SwitchgearNetwork = new();
+
+        public SwitchgearComponentType ComponentType => SwitchgearComponentType.Breaker;
+
+        PseudoNetwork<ISwitchgearComponent> IPseudoNetworkMember<ISwitchgearComponent>.Network { get; }
+
+        public IEnumerable<Connection>  Connections
+        {
+            get
+            {
+                foreach (var openEnd in this.OpenEnds)
+                    // set connections to LandingPad type in unity to mark them for replacement with new connection type
+                    if (openEnd.ConnectionType == NetworkType.LandingPad || openEnd.ConnectionType == MyNetworkType.ConnectionType)
+                        yield return openEnd;
+            }
+        }
+
+        public void OnMembersChanged()
+        {
+
+        }
+
+        void IPseudoNetworkMember<ISwitchgearComponent>.OnMemberAdded(ISwitchgearComponent member)
+        {
+            
+        }
+
+        void IPseudoNetworkMember<ISwitchgearComponent>.OnMemberRemoved(ISwitchgearComponent member)
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Prefabs/ISwitchgearComponent.cs
@@ -0,0 +1,17 @@
+﻿using LibConstruct;
+
+namespace ReVolt.Interfaces
+{
+    public interface ISwitchgearComponent : IPseudoNetworkMember<ISwitchgearComponent>
+    {
+        public enum SwitchgearComponentType
+        {
+            Breaker,
+            Decorative,
+            Power,
+            Data
+        }
+
+        public SwitchgearComponentType ComponentType { get; }
+    }
+}
