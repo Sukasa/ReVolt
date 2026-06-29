c06e6f2d7f8299b4a4139fbe6a6a03d8a3956662
sukasa <nick.m@shaw.ca>
2026-06-28 19:11:54 -0700

    More APC patches

--- Assets/Scripts/Patches/AreaPowerControllerPatches.cs
+++ Assets/Scripts/Patches/AreaPowerControllerPatches.cs
@@ -1,15 +1,16 @@
-﻿using Assets.Scripts.Networks;
+﻿using System;
+using Assets.Scripts.Networks;
 using Assets.Scripts.Objects.Electrical;
 using HarmonyLib;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace ReVolt.Patches
 {
     [HarmonyPatch(typeof(AreaPowerControl))]
     internal class AreaPowerControllerPatches
     {
-        [HarmonyPrefix]
-        [HarmonyPatch(nameof(AreaPowerControl.ReceivePower))]
+        [HarmonyPrefix, HarmonyPatch(nameof(AreaPowerControl.ReceivePower))]
         public static bool ReceivePowerPatch(CableNetwork cableNetwork, float powerAdded, AreaPowerControl __instance, ref float ____powerProvided)
         {
             if (!ReVolt.enableAreaPowerControlFix.Value)
@@ -26,17 +27,25 @@ namespace ReVolt.Patches
             if (powerAdded <= 0.0f)
                 return false;
 
-            // Try to satisfy power load
-            ____powerProvided -= powerAdded;
+            // Power that was drawn last tick should be removed from the incoming packet
+            if (____powerProvided > powerAdded)
+            {
+                ____powerProvided -= powerAdded;
+                powerAdded = 0;
+            }
+            else
+            {
+                powerAdded -= ____powerProvided;
+                ____powerProvided = 0;
+            }
 
             // If we didn't make up enough power to charge the battery or if there's no battery or if it's fully charged, then just return now and discard the remaining power
-            if (____powerProvided >= 0.0 || !(bool)__instance.Battery || __instance.Battery.IsCharged)
+            if (powerAdded <= 0.0 || !(bool)__instance.Battery || __instance.Battery.IsCharged)
                 return false;
 
             // Otherwise we charge the battery at max rate or as far as we can based on remaining power
             float num = Mathf.Min(__instance.Battery.PowerDelta, __instance.BatteryChargeRate, powerAdded);
             __instance.Battery.PowerStored += num; // Add to battery
-            ____powerProvided += num; // ... Add to the power we've provided to downstream?  I don't think we need this anymore.
 
             return false;
         }
@@ -47,21 +56,49 @@ namespace ReVolt.Patches
         {
             if (!ReVolt.enableAreaPowerControlFix.Value)
                 return true;
-            
+
             if (cableNetwork != __instance.OutputNetwork)
                 return false;
-            
-            if (__instance.Battery && !__instance.Battery.IsEmpty)
+
+            if (____powerProvided == 0)
             {
-                float num = Mathf.Min(__instance.Battery.PowerStored, powerUsed);
-                __instance.Battery.PowerStored -= num;
-                powerUsed -= num;
+                // APC is being fed enough power that the upstream is fully supplying it each tick
+                // So this tick we won't immediately draw from the battery and instead mark down the power use for next tick
+            }
+            else
+            {
+                // We didn't get enough power last tick so we need to draw from battery
+                if (__instance.Battery && !__instance.Battery.IsEmpty)
+                {
+                    var num = Mathf.Min(__instance.Battery.PowerStored, powerUsed);
+                    __instance.Battery.PowerStored -= num;
+                    powerUsed -= num;
+                }
             }
             
+            
+
             ____powerProvided += powerUsed;
 
             return false;
         }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(AreaPowerControl.AvailablePower), MethodType.Getter)]
+        public static bool AvailablePowerGetterPatch(AreaPowerControl __instance, ref float __result, float ____powerProvided)
+        {
+            if (!ReVolt.enableAreaPowerControlFix.Value)
+                return true;
+            
+            var availablePower = __instance.InputNetwork?.PotentialLoad ?? 0.0f;
+            __result = Math.Max(0.0f, availablePower - ____powerProvided - __instance.UsedPower);
+            
+            if (__instance.Battery != null && !__instance.Battery.IsEmpty)
+                __result += __instance.Battery.PowerStored;
+
+            return false;
+        }
+        
 
         [HarmonyPrefix]
         [HarmonyPatch(nameof(AreaPowerControl.GetUsedPower))]
@@ -79,7 +116,7 @@ namespace ReVolt.Patches
             if (__instance.OnOff)
             {
                 usedPower += __instance.UsedPower;
-                
+
                 if (__instance.OutputNetwork != null)
                     usedPower += ____powerProvided;
 
@@ -90,6 +127,5 @@ namespace ReVolt.Patches
             __result = usedPower;
             return false;
         }
-
     }
 }
