63fa4cc87881ecfed3c6b0945014572e1b7d4321
sukasa <nick.m@shaw.ca>
2026-07-04 16:17:30 -0700

    Updaet to 1.6.6

--- Assets/Scripts/ReVolt.cs
+++ Assets/Scripts/ReVolt.cs
@@ -36,7 +36,7 @@ namespace ReVolt
         internal static ConfigEntry<bool> enableAreaPowerControlFix;
         internal static ConfigEntry<bool> enableBatteryLimitsPatch;
 
-        public static readonly Mod MOD = new("Re-Volt", "1.6.5");
+        public static readonly Mod MOD = new("Re-Volt", "1.6.6");
 
         [UsedImplicitly]
         public void OnLoaded(ConfigFile config, List<GameObject> prefabs)
