fe330bf6a1dc9cc1302e2bba6f3e574d91a74b71
sukasa <nick.m@shaw.ca>
2026-07-11 21:24:33 -0700

    Patch spraying cables so that trays recalculate networks after spraying.  Tested compatibility with network painter mod

--- Assets/Scripts/Patches/ThingPatches.cs
+++ Assets/Scripts/Patches/ThingPatches.cs
@@ -1,5 +1,13 @@
-﻿using Assets.Scripts.Objects;
+﻿using System;
+using System.Collections.Generic;
+using Assets.Scripts;
+using Assets.Scripts.GridSystem;
+using Assets.Scripts.Objects;
+using Assets.Scripts.Objects.Electrical;
+using Assets.Scripts.Objects.Items;
+using Cysharp.Threading.Tasks;
 using HarmonyLib;
+using JetBrains.Annotations;
 using UnityEngine;
 
 namespace ReVolt.Patches
@@ -7,7 +15,6 @@ namespace ReVolt.Patches
     [HarmonyPatch(typeof(Thing))]
     public class ThingPatches
     {
-
         [HarmonyPostfix]
         [HarmonyPatch("HasState")]
         public static void FixStateIssues(string stateName, ref Interactable interactable, Thing __instance, ref bool __result)
@@ -17,7 +24,7 @@ namespace ReVolt.Patches
             // Bugfix: original code returned InteractButton1 instead of InteractButton3
             if (num == Interactable.Button3State && __instance.HasButton3State)
                 interactable = __instance.InteractButton3;
-            
+
             // Feature add: support Button4 and Button5 states
             if (num == Animator.StringToHash("Button4"))
             {
@@ -41,6 +48,65 @@ namespace ReVolt.Patches
             {
                 interactable = __instance.GetInteractable(InteractableType.Slot2);
                 __result = interactable != null;
+            }
+        }
+
+        // Sprayer fix - patches where Thing calls ISprayer.DoSpray() because I cannot directly patch an interface static method in harmony
+
+        [HarmonyTranspiler, HarmonyPatch(nameof(Thing.AttackWith))]
+        public static IEnumerable<CodeInstruction> RenderPatch(IEnumerable<CodeInstruction> instructions)
+        {
+            var BaseFunc = AccessTools.Method(typeof(ISprayer), nameof(ISprayer.DoSpray));
+            var InjectFunc = SymbolExtensions.GetMethodInfo(() => DoSprayPostfix(null, null, false));
+
+            foreach (var instruction in instructions)
+            {
+                if (instruction.Calls(BaseFunc))
+                    instruction.operand = InjectFunc;
+
+                yield return instruction;
+            }
+        }
+
+        [UsedImplicitly] // Transpiled postfix on an interface static method
+        public static Thing.DelayedActionInstance DoSprayPostfix(Thing thing, ISprayer sprayer, bool doAction)
+        {
+            var result = ISprayer.DoSpray(thing, sprayer, doAction);
+
+            if (thing is not Cable cable || !doAction || !GameManager.RunSimulation || result.IsDisabled)
+                return result;
+
+            Span<SmallCellRef> cableTrays = new SmallCellRef[6];
+            var count = 0;
+            cable.FillConnected<CableTray>(cableTrays, ref count);
+
+            if (count > 0) // Defer the repatch so that the network painter / whatever else have a chance to finish
+                DeferredRepatch(cable).Forget();
+
+            return result;
+        }
+
+        public static async UniTaskVoid DeferredRepatch(Cable cable)
+        {
+            await UniTask.Yield();
+
+            Operation();
+            return;
+
+            unsafe void Operation() // Local function wrapper to satisfy no-span-in-async compiler constraint
+            {
+                Span<SmallCellRef> cells = stackalloc SmallCellRef[6];
+                var count = 0;
+                cable.FillConnected<CableTray>(cells, ref count);
+
+                for (var idx = count - 1; idx >= 0; idx--)
+                {
+                    var tray = cells[idx].Get<CableTray>();
+                    if (tray is null) continue;
+
+                    tray.OnMemberRemoved(null);
+                    tray.OnMembersChanged();
+                }
             }
         }
     }
